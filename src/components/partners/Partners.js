9293fcf344047d2f8506f31f316ac595ba09aa67
Faceless1998 <81959592+Faceless1998@users.noreply.github.com>
2021-08-13 16:11:38 +0400

    kevin

--- src/components/partners/Partners.js
+++ src/components/partners/Partners.js
@@ -10,14 +10,23 @@ export const Partners = () => {
             </h1>
             <div className="heightcontroller automarginizer gradient widthcontroller opacity-25 margincontrollerpy-0 rounded-t" />
             <div class="flex-row">
-  <div class="card"></div>
-  <div class="card"></div>
-  <div class="card"></div>
-  <div class="card"></div>
-  <div class="card"></div>
-  <div class="card"></div>
-  <div class="card"></div>
-  <div class="card"></div>
+  <div class="card card1"></div>
+  <div class="card card2"></div>
+  <div class="card card3"></div>
+  <div class="card card4"></div>
+  <div class="card card5"></div>
+  <div class="card card6"></div>
+  <div class="card card7"></div>
+  <div class="card card8"></div>
+  <div class="card card9"></div>
+  <div class="card card10"></div>
+  <div class="card card11"></div>
+  <div class="card card12"></div>
+  <div class="card card13"></div>
+  <div class="card card14"></div>
+  <div class="card card15"></div>
+  <div class="card card16"></div>
+  <div class="card card17"></div>
 </div>
         </div>
     )
