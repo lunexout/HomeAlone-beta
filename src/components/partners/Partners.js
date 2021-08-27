efeb2f4746d84d999bcb490e3e010a8a70fdae27
Faceless1998 <81959592+Faceless1998@users.noreply.github.com>
2021-08-27 20:31:28 +0400

    new

--- src/components/partners/Partners.js
+++ src/components/partners/Partners.js
@@ -1,10 +1,6 @@
 import React from 'react'
 import './Partners.css'
 import './Partners.scss'
-
-
-
-// import { Footer } from './../footer/Footer'
 import PartnerWith from './partnetwith'
 import { Footer } from '../footer/Footer'
 
