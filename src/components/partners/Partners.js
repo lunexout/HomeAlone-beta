cf5faa136efff4dc3db67e8567d468daf5a2d860
Jano Gazashvili <69036038+lunexout@users.noreply.github.com>
2021-08-27 20:21:28 +0400

    fixing warnings

--- src/components/partners/Partners.js
+++ src/components/partners/Partners.js
@@ -4,50 +4,48 @@ import './Partners.scss'
 
 
 
-import IMG14 from './images/7th heaven.jpg'
-import IMG2  from './images/Alley palace.jpg'
-import IMG3 from './images/aquamarine.jpg'
-import IMG4  from './images/arfi group.jpg'
-import IMG5 from './images/Batumi palace.jpg'
-import IMG6  from './images/Black sea panorama.jpg'
-import IMG7 from './images/citron.jpg'
-import IMG8  from './images/gumbati group.jpg'
-import IMG9 from './images/guru holding.jpg'
-import IMG10  from './images/like house.jpg'
-import IMG11 from './images/calligraphy tower.jpg'
-import IMG12  from './images/mjm group.jpg'
-import IMG13 from './images/nbg.jpg'
-import IMG1  from './images/al mare batumi.jpg'
-import IMG15 from './images/premium group.jpg'
-import IMG16  from './images/real palace.jpg'
-import IMG17  from './images/star palace.jpg'
-import IMG18  from './images/sunrise development.jpg'
-import { Link } from 'react-router-dom'
-import { useTranslation } from 'react-i18next';
+// import IMG14 from './images/7th heaven.jpg'
+// import IMG2  from './images/Alley palace.jpg'
+// import IMG3 from './images/aquamarine.jpg'
+// import IMG4  from './images/arfi group.jpg'
+// import IMG5 from './images/Batumi palace.jpg'
+// import IMG6  from './images/Black sea panorama.jpg'
+// import IMG7 from './images/citron.jpg'
+// import IMG8  from './images/gumbati group.jpg'
+// import IMG9 from './images/guru holding.jpg'
+// import IMG10  from './images/like house.jpg'
+// import IMG11 from './images/calligraphy tower.jpg'
+// import IMG12  from './images/mjm group.jpg'
+// import IMG13 from './images/nbg.jpg'
+// import IMG1  from './images/al mare batumi.jpg'
+// import IMG15 from './images/premium group.jpg'
+// import IMG16  from './images/real palace.jpg'
+// import IMG17  from './images/star palace.jpg'
+// import IMG18  from './images/sunrise development.jpg'
 
 // import { Footer } from './../footer/Footer'
 import PartnerWith from './partnetwith'
 import { Footer } from '../footer/Footer'
-const imageJSON = [
-    {src: IMG1},
-    {src: IMG2},
-    {src: IMG3},
-    {src: IMG4},
-    {src: IMG5},
-    {src: IMG6},
-    {src: IMG7},
-    {src: IMG8},
-    {src: IMG9},
-    {src: IMG10},
-    {src: IMG11},
-    {src: IMG12},
-    {src: IMG13},
-    {src: IMG14},
-    {src: IMG15},
-    {src: IMG16},
-    {src: IMG17},
-    {src: IMG18}
-]
+// const imageJSON = [
+//     {src: IMG1},
+//     {src: IMG2},
+//     {src: IMG3},
+//     {src: IMG4},
+//     {src: IMG5},
+//     {src: IMG6},
+//     {src: IMG7},
+//     {src: IMG8},
+//     {src: IMG9},
+//     {src: IMG10},
+//     {src: IMG11},
+//     {src: IMG12},
+//     {src: IMG13},
+//     {src: IMG14},
+//     {src: IMG15},
+//     {src: IMG16},
+//     {src: IMG17},
+//     {src: IMG18}
+// ]
 
 export const Partners = () => {
     return (
