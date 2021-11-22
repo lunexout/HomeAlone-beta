16b6961c476831da4c16cb3a8535c34164bf0484
Faceless1998 <kakhidze.k@yahoo.com>
2021-11-22 17:51:35 +0400

    news1

---
+++ src/components/home/Shower.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import './Shower.css'
+export const Shower = () => {
+    return (
+        <div>
+            <div className="yt1__container">
+                <div className="yt1__sec10 d__sec s__bg lazy">
+                    <div className="content">
+                        <div className="item1">
+                            <a href="http://homealone.ge">
+                                <div className="icon" />
+                                <em />
+                            </a>
+                            <div className="pic" />
+                        </div>
+                        <div className="item2">
+                            <a href="http://localhost:3000/products/615b5d6c14b1962f14b41f26" target="_blank" rel="noreferrer">
+                                <div className="icon" />
+                                <em />
+                            </a>
+                            <div className="pic" />
+                        </div>
+                        <div className="item3">
+                            <a href="http://homealone.ge">
+                                <div className="icon" />
+                                <em />
+                            </a>
+                            <div className="pic" />
+                        </div>
+                        <div className="item4">
+                            <a href="http://homealone.ge">
+                                <div className="icon" />
+                                <em />
+                            </a>
+                            <div className="pic" />
+                        </div>
+                        <div className="item5">
+                            <a href="http://homealone.ge">
+                                <div className="icon" />
+                                <em />
+                            </a>
+                            <div className="pic" />
+                        </div>
+                        <div className="item6">
+                            <a href="http://homealone.ge">
+                                <div className="icon" />
+                                <em />
+                            </a>
+                            <div className="pic" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
