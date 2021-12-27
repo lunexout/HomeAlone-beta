e41b1cc5f165ed04489da3d3b1089446eacebabf
Faceless1998 <81959592+Faceless1998@users.noreply.github.com>
2021-12-27 17:29:03 +0400

    next 2

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -96,10 +96,10 @@ export const Header = () => {
                     </div>
                     
                 </div>
-                <div onMouseEnter={() => OpenBox(0)}>
-                        <a className='socialphoto' href="https://www.facebook.com/homealoneltd"><img src={imagefb} style={{ height: 30, width: 'auto', marginLeft: '10px', lineHeight:'30', verticalAlign:'middle'}} alt='' /></a>
+                <div>
+                        <a className='socialphoto' href="https://www.facebook.com/homealoneltd"><img src={imagefb} style={{ height: 30, width: 'auto', marginLeft: '20px', lineHeight:'30', verticalAlign:'middle'}} alt='' /></a>
                        
-                <a className='socialphoto'  href="https://www.instagram.com/ltdhome.alone/"> <img src={imageins} style={{ height: 30, width: 'auto', marginLeft: '10px', lineHeight:'30', verticalAlign:'middle' }} alt='' /></a>
+                <a className='socialphoto'  href="https://www.instagram.com/ltdhome.alone/"> <img src={imageins} style={{ height: 30, width: 'auto', marginLeft:0, lineHeight:'30', verticalAlign:'middle' }} alt='' /></a>
                     </div>
             </div>
             <div>
